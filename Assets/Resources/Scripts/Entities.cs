f127ed79277597137abb28235c061a0c053ae2bd
darthmorf <sampoirier@email.com>
2019-03-29 18:13:49 +0000

    Movement

---
+++ Assets/Resources/Scripts/Entities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity
+{
+    public bool move = true;
+    public bool player = false;
+    public GameObject go;
+    public Vector2 destination;
+    public float speed = 6f;
+
+    public Entity(string entityName, string spriteFile)
+    {
+        go = new GameObject();
+        go.name = entityName;
+        SpriteRenderer sr = go.AddComponent<SpriteRenderer>();
+        sr.sprite = Resources.Load<Sprite>(spriteFile);
+        destination = go.transform.position;
+    }
+}
---
+++ Assets/Resources/Scripts/StateManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour {
+
+    public Entity player = null;
+    
+    float squareSize = 0.8f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        player = new Entity("Yaga", "Sprites/yaga");
+
+        //Entity temp = new Entity("Yaga2", "Sprites/yaga");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Movement();	
+	}
+
+    void Movement ()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+           player.destination.y += squareSize;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            player.destination.y -= squareSize;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            player.destination.x += squareSize;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            player.destination.x -= squareSize;
+        }
+
+        player.go.transform.position = Vector2.Lerp(player.go.transform.position, player.destination, player.speed * Time.deltaTime);
+        //player.go.transform.position = position;
+    }
+}
